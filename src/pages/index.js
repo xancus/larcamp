8b53034a53d5701f89856156313c52f7dd4ffefc
David Matos <davidxanco@gmail.com>
2023-05-21 11:42:58 +0200

    Add named procuts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Header from '@/components/Header'
 import { Inter } from 'next/font/google'
 import CategoriesMenu from '@/components/CategoriesMenu'
 import Footer from '@/components/Footer'
-import { products as initialProducts } from '../../db/products.json'
 import { useState } from 'react'
 import useFilter from '../hooks/useFilter'
+import productsData from '../../db/products.json'
 
 const font = Inter({
   weight: ['400', '700'],
@@ -14,7 +14,7 @@ const font = Inter({
 
 export default function Home() {
   // const category = 'all'
-  const [products] = useState(initialProducts)
+  const [products] = useState(productsData.products)
   const { filterProducts } = useFilter()
   const filteredProducts = filterProducts(products)
 
